abd023abf9b5b5d0ba4be71f6ff827a846fd1d6e
unknown <slava3113@rambler.ru>
2021-08-30 13:40:44 +0600

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $(".top-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+  });
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
+});
